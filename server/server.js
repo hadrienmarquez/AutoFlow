1e6f9874338ee1bbd956b472f56400227302a6bf
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-17 11:24:26 +0100

    Adding dependencies to project

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
-import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import * as loaders from "./loaders/loaders.js";
+import * as services from "./services/services.js";
+import { promptConfirm } from "./scripts/promptScript.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log("Starting process...");
+const tyFlowUrl = "http://docs.tyflow.com/download/";
 
-const server = express();
+async function daemon() {
+  let browserInstance = await loaders.loadBrowser();
+  let pageInstance = await loaders.loadPage(browserInstance);
 
-const PORT = process.env.PORT || 8080;
+  if (await services.checkVersion(pageInstance, tyFlowUrl)) {
+    // prompt things with inquirer.....
+    if (await promptConfirm()) {
+      // DL stuff
+      console.log("CURRENT");
+      let skipped = false;
+      let cur_version = new_version;
+    } else {
+      console.log("SKIPPED");
+      let skipped = true;
+      let skipped_version = new_version;
+    }
+  }
+}
 
-server.use(express.static(path.join(__dirname, "client")));
+daemon();
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// promptConfirm().then((ret) => {
+//   console.log(ret);
+// });
