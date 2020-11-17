4c357ea0438adccebaea9c1626328a8496957e07
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-17 12:47:44 +0100

    Main daemon without node_schedule cron job support

--- server/server.js
+++ server/server.js
@@ -1,35 +1,27 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import * as loaders from "./loaders/loaders.js";
 import * as services from "./services/services.js";
 import { promptConfirm } from "./scripts/promptScript.js";
+import { UpdateEmitter } from "./subscribers/UpdateEmitter.js";
+import env from "./config/index.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log("Starting process...");
 const tyFlowUrl = "http://docs.tyflow.com/download/";
+const updateEmitter = new UpdateEmitter();
 
 async function daemon() {
   let browserInstance = await loaders.loadBrowser();
   let pageInstance = await loaders.loadPage(browserInstance);
 
   if (await services.checkVersion(pageInstance, tyFlowUrl)) {
-    // prompt things with inquirer.....
     if (await promptConfirm()) {
-      // DL stuff
-      console.log("CURRENT");
-      let skipped = false;
-      let cur_version = new_version;
+      console.log("CURRENT - Starting archive & downloads.....");
+      updateEmitter.emit("update", false);
     } else {
-      console.log("SKIPPED");
-      let skipped = true;
-      let skipped_version = new_version;
+      console.log("SKIPPED - Only updating database");
+      updateEmitter.emit("skip", true);
     }
   }
 }
 
-daemon();
+console.log("Starting process...");
 
-// promptConfirm().then((ret) => {
-//   console.log(ret);
-// });
+daemon();
