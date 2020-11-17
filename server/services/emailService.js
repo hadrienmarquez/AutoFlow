f983b6814134f2205808ba8b9b0df1d630693e5c
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-17 11:34:12 +0100

    Configuring Nodemailer to work with google account through OAuth authentification

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,41 +1,58 @@
 "use strict";
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 import env from "../config/index.js";
+const OAuth2 = google.auth.OAuth2;
 
 function writeMail(data) {
+  const cudaText =
+    data.new_cuda == data.current_cuda
+      ? `<p> CUDA is up to date : ${data.new_cuda} </p>`
+      : `<p> CUDA has a new version : ${data.new_cuda} </p>`;
+
   return `<h2> TyFlow has a new version available! </h2>
     <p><b>New version:</b> ${data.new_version}</p>
     <p><b>Old version:</b> ${data.current_version}</p>
+    ${cudaText}
     <p>Go to machine@machine to finish the update!</p>`;
 }
 
 function sendEmail(data) {
-  let transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+  const oauth2Client = new OAuth2(
+    env.auth_client_id,
+    env.auth_client_secret,
+    env.auth_redirect_url
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: env.auth_refresh_token,
+  });
+
+  const accessToken = oauth2Client.getAccessToken();
+
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-      user: "da3ed0b6a4532f",
-      pass: "5a115d8200c03a",
+      type: "OAuth2",
+      user: env.email_adress,
+      clientId: env.auth_client_id,
+      clientSecret: env.auth_client_secret,
+      refreshToken: env.auth_refresh_token,
+      accessToken: accessToken,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
-  //   let transport = nodemailer.createTransport({
-  //     host: "ssl0.ovh.net",
-  //     port: 2525,
-  //     auth: {
-  //       user: "it@st-louis.tv",
-  //       pass: env.email_password,
-  //     },
-  //   });
-
-  let mailOptions = {
-    from: '"AutoFlow" <it@st-louis.tv>',
-    to: "arthur.georget@st-louis.tv, tibor.bernard@st-louis.tv",
+  const mailOptions = {
+    from: env.email_adress,
+    to: "hadrien195@hotmail.fr",
     subject: "AutoFlow@Bot - New Version",
     html: writeMail(data),
   };
 
-  transport.sendMail(mailOptions, (error, info) => {
+  smtpTransport.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log(`Couldn't send email => ${error}`);
     }
