b6e754e66348a5c42877ab0c7409286a81cca84e
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-17 11:36:57 +0100

    Subscribers to the update version and skip version events

---
+++ server/subscribers/UpdateEmitter.js
@@ -0,0 +1,50 @@
+import { EventEmitter } from "events";
+import { tyflowArchive, tyflowDownload } from "../services/downloadService.js";
+import env from "../config/index.js";
+import { readData, writeData } from "../models/versionModelController.js";
+
+class UpdateEmitter extends EventEmitter {
+  constructor() {
+    super();
+    this.on("update", this.download);
+    this.on("update", this.dbUpdate);
+    this.on("skip", this.dbUpdate);
+  }
+
+  async download() {
+    await tyflowArchive();
+    await tyflowDownload();
+  }
+
+  async dbUpdate(skipped) {
+    let cache;
+
+    try {
+      cache = await readData("/usr/src/app/server/models/cache.json");
+    } catch (error) {
+      console.log(`Couldn't access cache => ${error}`);
+    }
+    let data;
+    if (skipped) {
+      data = {
+        current: { version: cache.current_version, skipped: true },
+        skipped: { version: cache.new_version },
+        cuda: { date: cache.new_cuda },
+      };
+    } else {
+      data = {
+        current: { version: cache.new_version, skipped: false },
+        skipped: { version: cache.new_version },
+        cuda: { date: cache.new_cuda },
+      };
+    }
+
+    try {
+      await writeData(data, "/usr/src/app/server/models/versions.json");
+    } catch (error) {
+      error;
+    }
+  }
+}
+
+export { UpdateEmitter };
